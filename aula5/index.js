9576ec1dd5aafb72c94fd789eb9d24074afef445
Guilherme Freitas <gui2010mg68@gmail.com>
2025-07-08 21:02:16 -0300

    estudo de variaveis

---
+++ aula5/index.js
@@ -0,0 +1,2 @@
+console.log('Ola Mundo')
+console.log('Este trecho será exibido no console do navegador.')
---
+++ aula6/index.js
@@ -0,0 +1,15 @@
+/**
+ * Não podemos criar variaveis com palavras  reservadas
+ * Variaveis precisam ter nomes significativos
+ * Não pode começar o nome de uma variavel com um nmero
+ * Não ppodem conter espaços ou traços
+ * Utilizamos camelCase
+ * Case-sensitive
+ * Não podemos redeclarar variaveis com let
+ */
+let nome = 'João'; // string
+
+console.log(`${nome} nasceu em 1984`)
+console.log(`Em 2000 ${nome} conheceu Maria`)
+console.log(`${nome} casou-se com Maria em 2012`)
+console.log(`O filho de ${nome} se chama Eduardo`)
