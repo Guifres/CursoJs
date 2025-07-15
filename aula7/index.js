1407530b437b7974d03c7aeecaf8900893a1bb0b
Guilherme Freitas <gui2010mg68@gmail.com>
2025-07-15 19:41:28 -0300

    aula7

--- aula7/index.js
+++ aula7/index.js
@@ -9,6 +9,11 @@
  * Não podemos redeclarar variaveis com let
  */
 
-const nome = 'João';
-console.log(nome);
- 
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero
+const resultadoDuplicado = resultado * 2
+let resultadoTriplicado = resultado * 3
+console.log(typeof resultadoDuplicado);
+
+  
