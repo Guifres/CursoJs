4faa74777100e252045c14cf0c98ef6ac1fb6764
Guilherme Freitas <gui2010mg68@gmail.com>
2025-07-18 15:43:05 -0300

    ,

--- aula9/index.js
+++ aula9/index.js
@@ -1,3 +1,4 @@
+// ECMAScript 2015 (ES6) é JavaScript
 var nome = 'Guilherme'; 
 var nome = ' Pedro'
 console.log(nome);
---
+++ aula9/index1.js
@@ -0,0 +1,10 @@
+const nome = 'Luiz'; // String
+const nome1 = "Luiz"; // String
+const nome2 = `Luiz`; // String
+const num1 = 10; // Number
+const num2 = 10.52; // Number
+let nomeAluno ; // undefined -> Não aponta pra local nenhuma na memoria
+const sobrenomeAluno = null; // Nulo _> não aponta pra local nenhuma na memoria 
+const aprovado = false; // Bolean = true, false (logico)
+
+
