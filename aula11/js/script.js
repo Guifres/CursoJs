2494b01c2f7a8c019cb65e83be880216c2f093d1
Guilherme Freitas <gui2010mg68@gmail.com>
2025-08-06 18:43:13 -0300

    .

---
+++ aula11/js/script.js
@@ -0,0 +1,6 @@
+const num1 = prompt('Digite um Numero');
+const num2 = prompt('Digite putro Numero');
+num1 = Number(num1)
+num2 = Number(num2)
+const resultado = num1 + num2
+window.alert(resultado)
