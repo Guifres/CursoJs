70fd559785392bdcae719fb5bc00f38b3f55b40d
Guilherme Freitas <gui2010mg68@gmail.com>
2025-08-18 20:23:14 -0300

    .

--- aula13/ex/Js/script.js
+++ aula13/ex/Js/script.js
@@ -1,11 +1,11 @@
 const nome = prompt('Digite seu nome')
 document.body.innerHTML += `Seu nome é: ${nome} <br/>`
 document.body.innerHTML += `Seu nome tem ${nome.length} letras <br/>`
-document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]} `
-document.body.innerHTML += `Qual o primeiro índice da letra a no seu nome? <br/>`
-document.body.innerHTML += `Qual o ultimo indice da letra a no seu nome? <br/>`
-document.body.innerHTML += `As ultimas 3 letras do seu nome são: ${nome[-3]} <br/>`
-document.body.innerHTML += `As palavras do seu nome são: <br/>`
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]} <br/>`
+document.body.innerHTML += `Qual o primeiro índice da letra L no seu nome? ${nome.indexOf('l')} <br/>`
+document.body.innerHTML += `Qual o ultimo indice da letra H no seu nome? ${nome.lastIndexOf('h')} <br/>`
+document.body.innerHTML += `As ultimas 3 letras do seu nome são: ${nome.slice(-3)} <br/>`
+document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ')} <br/>`
 document.body.innerHTML += `Seu nome com letras maiusculas: ${nome.toUpperCase()} <br/>`
 document.body.innerHTML += `Seu nome com letras minusculas: ${nome.toLowerCase()} <br/>`
 
