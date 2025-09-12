cbe41b5fc669fb92fad229ebe3ad0fb5038f9671
Guilherme Freitas <gui2010mg68@gmail.com>
2025-09-12 11:13:10 -0300

    .

--- aula14/index.js
+++ aula14/index.js
@@ -3,4 +3,6 @@ let num2 = 2;
 /*console.log (num.1.toString() + num2) */
 //console.log(num1.toString(2))
 //console.log(num1.toFixed(2)) para Formatação 
-console.log(s 
+//console.log(Number.isInteger(num1)) validar se o numero é inteiro
+// console.log(Number.isNaN(temp))
+//let temp = num1 * "ola";
