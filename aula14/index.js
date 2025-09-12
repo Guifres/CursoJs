fa584c67732081ba58fafffa920da48b213c2e91
Guilherme Freitas <gui2010mg68@gmail.com>
2025-09-12 10:49:33 -0300

    .

--- aula14/index.js
+++ aula14/index.js
@@ -1,4 +1,6 @@
-let num1 = 1;
+let num1 = 10;
 let num2 = 2;
-
-console.log(num1 + num2)
+/*console.log (num.1.toString() + num2) */
+//console.log(num1.toString(2))
+//console.log(num1.toFixed(2)) para Formatação 
+console.log(s 
