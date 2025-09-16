f279f4d8bb9515b122d16938988c76b7eff152bc
Guilherme Freitas <gui2010mg68@gmail.com>
2025-09-15 21:10:27 -0300

    Update index.js

--- aula23/index.js
+++ aula23/index.js
@@ -1,3 +1,9 @@
-/*
- 
- */
+const hora  = 10
+
+if (hora >= 0 && hora <= 11) {
+    console.log('Bom Dia')
+} else if (hora >= 12 && hora <= 17) {
+    console.log('BOa Tarde')
+} else if (hora >= 18 && hora <= 23){
+    console.log('Boa Noite')
+} 
