166042e1b194c69cbc3f816505f5ca444e427434
Guilherme Freitas <gui2010mg68@gmail.com>
2025-09-25 23:22:22 -0300

    .

---
+++ aula27/index.js
@@ -0,0 +1,4 @@
+// (comdição) ? 'Valor para TRUE' : 'Valor para False';
+const pontuacaoUsuario = 500;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : ' Usuario Normal';
+console.log(nivelUsuario);
--- exeaula25/assets/js/main.js
+++ exeaula25/assets/js/main.js
@@ -14,20 +14,3 @@ function setResultado (msg) {
     resultado.innerHTML = '';
 
 }
-
-
-
-
-function Calcular(){
-    let peso = document.getElementById('peso').value
-    let altura = document.getElementById('altura').value
-    let resultado = document.getElementById('resultado')
-    let Peso = Number(peso)
-    let Altura = Number(altura)
-    
-
-    let imc = Peso / (Altura * Altura)
-    
-
-    resultado.innerHTML = `<p>Seu IMC é ${imc}<p/>`
-}
