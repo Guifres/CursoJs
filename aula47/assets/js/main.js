add102502d9ee0c099337f176c13aa924e0dbeda
Guilherme Freitas <gui2010mg68@gmail.com>
2026-01-05 19:19:55 -0300

    .

--- aula47/assets/js/main.js
+++ aula47/assets/js/main.js
@@ -1,17 +1,41 @@
+function criaHoraDosSegundos (segundos) {
+    const data = new Date(segundos * 1000 );
+    return data.toLocaleTimeString('pt-BR' , {
+        hour12: false,
+        timeZone: 'UTC'
+    });
+
+}
 const relogio = document.querySelector('.relogio');
 const inicar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
+
+function iniciaRelogio(){
+    timer = setInterval(function(){
+        segundos ++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000 );
+
+}
+
 
 inicar.addEventListener('click', function(event){
-    alert('Cliquei no iniciar');
+    relogio.classList.remove('pausado')
+    clearInterval(timer);
+    iniciaRelogio();
 });
 
 pausar.addEventListener('click', function(event) {
-    alert('Cliquei no pausar')
+    clearInterval(timer);
+    relogio.classList.add('pausado')
 })
 
 zerar.addEventListener('click', function(event){
-    alert('Cliquei no zerar')
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
 
 })
