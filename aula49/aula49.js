e0c2d7f437889835f26a863318b1b9ca3fe46d02
Guilherme Freitas <gui2010mg68@gmail.com>
2026-01-28 21:54:48 -0300

    funções

--- aula49/aula49.js
+++ aula49/aula49.js
@@ -3,3 +3,27 @@ function falaoi() {
     console.log('Oie')
 }
 
+// First-class objects (Objetos de primeira classe)
+// Function expression
+
+const SouUmDado = function() {
+    console.log('Sou um dado. ')
+};
+
+SouUmDado();
+
+// Arrow function 
+
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function')
+}
+
+// Dentro de um objeto
+
+const obj = {
+    falar: function() {
+        console.log('Estou falando')
+    }
+};
+
+obj.falar()
