235452463e8903ef7039db0191a445cdbe1296e1
Guilherme Freitas <gui2010mg68@gmail.com>
2026-02-20 22:32:10 -0300

    .

---
+++ aula66/index.js
@@ -0,0 +1,8 @@
+// dobre os numeros
+const numeros = [5, 50, 80, 1, 2, 3, 4, 5, 8, 7, 11, 15, 22, 27];
+const numerosEmDobro = numeros.map(function(valor){
+    return valor * 2
+    
+})
+
+console.log(numerosEmDobro)
